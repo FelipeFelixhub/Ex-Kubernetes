99664158567bce0d95d4ade96fdceddf5e1664c9
Felipe Felix <115411900+FelipeFelixhub@users.noreply.github.com>
2024-04-16 00:40:51 +0000

    update app js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Olá, Kubernetes!');
+    res.send('Hello Fucking World!!');
 });
 
 const PORT = process.env.PORT || 3000;
